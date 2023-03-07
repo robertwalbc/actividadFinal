558beea270a26e0072feb024deaabb9b641a4c3d
fortis-a <andrea.fortisb@gmail.com>
2023-03-06 22:23:42 -0600

    changes

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -6,21 +6,23 @@ function Menu(){
 
   return(
     <div>
-      <nav className="navbar navbar-expand-md navbar-dark" id="navbar">
-        <div className="container-fluid">
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <div className="container">  
+          <img className="me-2" src="./img/logo.png" width="30"
+            height="30" alt="Logo" />                
           <Link to="/" className="navbar-brand">Actividad Evaluada</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
             aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse pb-2 pb-lg-0" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 me-lg-3">
                             
-              <li className="nav-item sub" id="celulares">
+              <li className="nav-item">
                 <Link to="/celulares" className="nav-link active">Celulares</Link>
               </li>
-              <li className="nav-item sub" id="juegos">
+              <li className="nav-item">
                 <Link to="/juegos" className="nav-link active">Juegos</Link>
               </li>
             </ul>
@@ -30,5 +32,4 @@ function Menu(){
     </div>
   )
 }
-
 export default Menu
